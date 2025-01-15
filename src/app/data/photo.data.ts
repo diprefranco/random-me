47e635ddd7e09b02f648076504dbf43fd4d5cdca
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-01-15 12:39:02 -0300

    Photo data: sample images data updated and photographerUrl added.

--- src/app/data/photo.data.ts
+++ src/app/data/photo.data.ts
@@ -4,37 +4,41 @@ export const photoData: Photo[] = [
   {
     src: 'img/kawe-rodrigues--ayoq1jzyKc-unsplash.jpg',
     orientation: 'horizontal',
-    description: 'A farmer showing their plants.',
-    fileName: 'kawe-rodrigues.jpg',
-    date: new Date(Date.UTC(2025, 0, 1)),
-    location: 'Rio de Janeiro',
-    photographer: 'John Doe'
+    description: 'An older man is walking through a garden',
+    fileName: 'casa-suzuki-older-man.jpg',
+    date: new Date(Date.UTC(2024, 11, 29)),
+    location: 'Tomé-Açu, State of Pará, Brazil',
+    photographer: 'Kawê Rodrigues',
+    photographerUrl: 'https://unsplash.com/@kawerodriguess'
   },
   {
     src: 'img/jalil-saeidi-23IRsBMvmuw-unsplash.jpg',
     orientation: 'horizontal',
-    description: 'A man with half face inside a swimming pool.',
-    fileName: 'jalil-saeidi.jpg',
-    date: new Date(Date.UTC(2022, 4, 14)),
-    location: 'New York',
-    photographer: 'Albert Doe'
+    description: 'A man is floating in a pool of water',
+    fileName: 'drown-in-memories.jpg',
+    date: new Date(Date.UTC(2024, 11, 21)),
+    location: 'New York, USA',
+    photographer: 'Jalil Saeidi',
+    photographerUrl: 'https://unsplash.com/@jalil_sd'
   },
   {
     src: 'img/polina-kuzovkova-fv16_AJYLJY-unsplash.jpg',
     orientation: 'vertical',
-    description: 'A woman in a temple.',
-    fileName: 'polina-kuzovkova.jpg',
-    date: new Date(Date.UTC(2019, 7, 12)),
-    location: 'India',
-    photographer: 'Margaret Doe'
+    description: 'A woman in a yellow and brown sari walking in front of a building',
+    fileName: 'woman-yellow-and-brown-sari-walking.jpg',
+    date: new Date(Date.UTC(2025, 0, 6)),
+    location: 'Bangkok, Thailand',
+    photographer: 'Polina Kuzovkova',
+    photographerUrl: 'https://unsplash.com/@p_kuzovkova'
   },
   {
     src: 'img/ritul-bhattacharjee-HBJEJEErmlc-unsplash.jpg',
     orientation: 'vertical',
-    description: 'A woman smiling kindly.',
-    fileName: 'ritul-bhattacharjee.jpg',
-    date: new Date(Date.UTC(2023, 2, 4)),
-    location: 'India',
-    photographer: 'Emily Doe'
+    description: 'Rajasthani women in their regular Rajasthani attire',
+    fileName: 'rajasthani-women-standing.jpg',
+    date: new Date(Date.UTC(2025, 0, 4)),
+    location: 'Jaipur, Rajasthan, India',
+    photographer: 'Ritul Bhattacharjee',
+    photographerUrl: 'https://unsplash.com/@rd_ritul7'
   }
 ];
