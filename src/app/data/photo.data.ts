789dd8d8e461fec9c257595312e590b61a3d1350
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-01-18 17:03:42 -0300

    Site files: folder structure updated with assets and random-images folders.

--- src/app/data/photo.data.ts
+++ src/app/data/photo.data.ts
@@ -2,7 +2,7 @@ import { Photo } from '../models/photo.model';
 
 export const photoData: Photo[] = [
   {
-    src: 'img/kawe-rodrigues--ayoq1jzyKc-unsplash.jpg',
+    src: 'random-images/kawe-rodrigues--ayoq1jzyKc-unsplash.jpg',
     orientation: 'horizontal',
     description: 'An older man is walking through a garden',
     fileName: 'casa-suzuki-older-man.jpg',
@@ -12,7 +12,7 @@ export const photoData: Photo[] = [
     photographerUrl: 'https://unsplash.com/@kawerodriguess'
   },
   {
-    src: 'img/jalil-saeidi-23IRsBMvmuw-unsplash.jpg',
+    src: 'random-images/jalil-saeidi-23IRsBMvmuw-unsplash.jpg',
     orientation: 'horizontal',
     description: 'A man is floating in a pool of water',
     fileName: 'drown-in-memories.jpg',
@@ -22,7 +22,7 @@ export const photoData: Photo[] = [
     photographerUrl: 'https://unsplash.com/@jalil_sd'
   },
   {
-    src: 'img/polina-kuzovkova-fv16_AJYLJY-unsplash.jpg',
+    src: 'random-images/polina-kuzovkova-fv16_AJYLJY-unsplash.jpg',
     orientation: 'vertical',
     description: 'A woman in a yellow and brown sari walking in front of a building',
     fileName: 'woman-yellow-and-brown-sari-walking.jpg',
@@ -32,7 +32,7 @@ export const photoData: Photo[] = [
     photographerUrl: 'https://unsplash.com/@p_kuzovkova'
   },
   {
-    src: 'img/ritul-bhattacharjee-HBJEJEErmlc-unsplash.jpg',
+    src: 'random-images/ritul-bhattacharjee-HBJEJEErmlc-unsplash.jpg',
     orientation: 'vertical',
     description: 'Rajasthani women in their regular Rajasthani attire',
     fileName: 'rajasthani-women-standing.jpg',
