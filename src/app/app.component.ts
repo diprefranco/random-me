dc74a830ee47d0f66b8a94a3d6503b2091b30e05
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-01-19 17:19:15 -0300

    Console welcome message added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ResourceService } from './services/resource.service';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,35 @@ import { HomeComponent } from './components/pages/home/home.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private resourceService = inject(ResourceService);
+
+  ngOnInit() {
+    const currentYear = new Date().getFullYear();
+    const projectUrl = this.resourceService.getProjectUrl();
+    this.logConsoleWelcomeMessage(currentYear, projectUrl);
+  }
+
+  private logConsoleWelcomeMessage(year: number, projectUrl: string) {
+    setTimeout(() => {
+      console.log(`%c
+                                                                                                                         
+  ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ███████╗███████╗██╗     ██╗      █████╗ ███████╗██╗  
+  ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ██╔════╝██╔════╝██║     ██║     ██╔══██╗██╔════╝██║  
+  ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      █████╗  █████╗  ██║     ██║     ███████║███████╗██║  
+  ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      ██╔══╝  ██╔══╝  ██║     ██║     ██╔══██║╚════██║╚═╝  
+  ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    ██║     ███████╗███████╗███████╗██║  ██║███████║██╗  
+   ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚═╝     ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  
+                                                                                                                         
+  ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗  
+  ║                      The site code you're exploring might be minimized.                                           ║  
+  ║                      Check it out here: ${projectUrl}                                  ║  
+  ║                                                                                                                   ║  
+  ║                                                © ${year} Franco Dipre                                                ║  
+  ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝  
+                                                                                                                         `,
+        "font-size: 14px; color: #05f903; background-color: #191919");
+    }, 1000);
+  }
 }
