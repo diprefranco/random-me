4c14a4cbe8538a9c7e13d72b0978abad57d84985
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-01-25 16:53:02 -0300

    Sample photos: rename folder and photo.data.ts to sample-photo.data.ts

--- src/app/data/sample-photo.data.ts
+++ src/app/data/sample-photo.data.ts
@@ -3,7 +3,7 @@ import { Photo } from '../models/photo.model';
 export const photoData: Photo[] = [
   {
     fileName: 'casa-suzuki-older-man.jpg',
-    src: 'random-images/kawe-rodrigues--ayoq1jzyKc-unsplash.jpg',
+    src: 'sample-photos/kawe-rodrigues--ayoq1jzyKc-unsplash.jpg',
     orientation: 'horizontal',
     description: 'An older man is walking through a garden',
     date: new Date(Date.UTC(2024, 11, 29)),
@@ -13,7 +13,7 @@ export const photoData: Photo[] = [
   },
   {
     fileName: 'drown-in-memories.jpg',
-    src: 'random-images/jalil-saeidi-23IRsBMvmuw-unsplash.jpg',
+    src: 'sample-photos/jalil-saeidi-23IRsBMvmuw-unsplash.jpg',
     orientation: 'horizontal',
     description: 'A man is floating in a pool of water',
     date: new Date(Date.UTC(2024, 11, 21)),
@@ -23,7 +23,7 @@ export const photoData: Photo[] = [
   },
   {
     fileName: 'woman-yellow-and-brown-sari-walking.jpg',
-    src: 'random-images/polina-kuzovkova-fv16_AJYLJY-unsplash.jpg',
+    src: 'sample-photos/polina-kuzovkova-fv16_AJYLJY-unsplash.jpg',
     orientation: 'vertical',
     description: 'A woman in a yellow and brown sari walking in front of a building',
     date: new Date(Date.UTC(2025, 0, 6)),
@@ -33,7 +33,7 @@ export const photoData: Photo[] = [
   },
   {
     fileName: 'rajasthani-women-standing.jpg',
-    src: 'random-images/ritul-bhattacharjee-HBJEJEErmlc-unsplash.jpg',
+    src: 'sample-photos/ritul-bhattacharjee-HBJEJEErmlc-unsplash.jpg',
     orientation: 'vertical',
     description: 'Rajasthani women in their regular Rajasthani attire',
     date: new Date(Date.UTC(2025, 0, 4)),
